d368a223c2b9d833d0a9049f2c4392a4d317ac52
Vladislav Shevchuk <mr2faced@proton.me>
2026-06-11 21:44:01 +0300

    Update vps-setup.sh

--- vps-setup.sh
+++ vps-setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
 # check root
 if [[ $EUID -ne 0 ]]; then
-   echo "Пожалуйста, запустите этот скрипт с правами root"
-   echo "Используйте sudo -i"
-   echo "Затем запустите скрипт повторно"
+   echo -e "${red}❌ Пожалуйста, запустите этот скрипт с правами root${plain}"
+   echo -e "${yellow}💡 Используйте sudo -i${plain}"
+   echo -e "${yellow}🔄 Затем запустите скрипт повторно${plain}"
    exit 1
 fi
+
+echo -e "${green}✅ Скрипт запущен от root пользователя. Продолжаем настройку...${plain}"
